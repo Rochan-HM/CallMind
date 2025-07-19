45270c600313f959914159f3743e50decef5b031
Gururaj Deshpande <66385690+guru-desh@users.noreply.github.com>
2025-07-19 13:37:25 -0700

    add chromadb_utils

---
+++ chromadb_utils/utils.py
@@ -0,0 +1,32 @@
+import chromadb
+import os
+from chromadbx import UUIDGenerator
+import chromadb.utils.embedding_functions as embedding_functions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = chromadb.CloudClient(
+    api_key=os.getenv("CHROMA_API_KEY"),
+    tenant=os.getenv("CHROMA_TENANT"),
+    database="testdb",
+)
+
+google_ef = embedding_functions.GoogleGenerativeAiEmbeddingFunction(
+    api_key=os.getenv("GEMINI_API_KEY"),
+)
+collection = client.get_or_create_collection("callmind", embedding_function=google_ef)
+
+
+def add_document(document: str):
+    collection.add(
+        documents=[document],
+        ids=UUIDGenerator().generate_uuid(),
+    )
+
+
+def retrieve_document(query: str, **kwargs):
+    return collection.query(
+        query_texts=[query],
+        **kwargs,
+    )
