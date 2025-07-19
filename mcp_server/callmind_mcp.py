7f7067b201cc7a7718a5e04fb980268af120401d
Gururaj Deshpande <66385690+guru-desh@users.noreply.github.com>
2025-07-19 14:27:55 -0700

    working mcp server

--- mcp_server/callmind_mcp.py
+++ mcp_server/callmind_mcp.py
@@ -1,4 +1,5 @@
 from fastmcp import FastMCP
+from pydantic import BaseModel
 import os
 import sys
 
@@ -9,10 +10,28 @@ from chromadb_utils.utils import retrieve_document
 
 mcp = FastMCP("Callmind 📞")
 
+
+class QueryCollectionResponse(BaseModel):
+    result: list[str]
+
+
 @mcp.tool
-def query_collection(query: str) -> str:
+def query_collection(query: str) -> QueryCollectionResponse:
     """Query the collection"""
-    return retrieve_document(query)
+    chroma_result = retrieve_document(query)
+
+    (
+        x := QueryCollectionResponse(
+            result=[
+                transcription[0]
+                for transcription in chroma_result["documents"]
+                if transcription[0] is not None
+            ]
+        )
+    )
+
+    return x
+
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    mcp.run(transport="http", host="0.0.0.0", port=9000)
